661e7028ebae301b3c3ad6e5c09a42863deeb4ef
LSDEV <120205647+leanderlombardi@users.noreply.github.com>
2023-01-15 18:48:19 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace HelloWorld {
+    //     class name
+    public class Program {
+        //            void name (type     name     )
+        public static void Main (string[] arguments) {
+            // type   name = value;
+               string test = "Hello, world!";
+               Console.WriteLine(test);
+        }
+    }
+}
